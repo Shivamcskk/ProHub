8a8018006145b7335780aa31b9809b4b126f689d
chaudhary shivam akhilesh <42630863+Shivamcskk@users.noreply.github.com>
2020-12-19 20:22:28 +0530

    commit

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -18,8 +18,7 @@ const ProfileGithub = ({username,getGithubRepos,repos}) => {
                 (
                     repos.map(repo=>(
                         <div key={repo.id} className='repo bg-white p-1 my-1'>
-                            <div>
-                                <h4>
+                            <div><h4>
                                     <a 
                                     href={repo.html_url}
                                     target='_blank'
