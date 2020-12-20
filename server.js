efc2f348ed76dee79d13bf6d34e8c06a37be4a16
chaudhary shivam akhilesh <42630863+Shivamcskk@users.noreply.github.com>
2020-12-20 14:23:48 +0530

    commit

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express =require('express');
 const App =express();
 const connectDB=require('./config/db');
+const path=require('path');
 
 
 connectDB();
 
 App.use(express.json({extended:false}))
 
-App.get('/',(req,res)=>res.send('Api Running'))
 
 //define Routes
 App.use('/api/users',require('./routes/api/users'));
@@ -15,6 +15,14 @@ App.use('/api/profile',require('./routes/api/profile'));
 App.use('/api/posts',require('./routes/api/posts'));
 App.use('/api/auth',require('./routes/api/auth'));
 
+if(process.env.NODE_ENV==='production')
+{
+    App.use(express.static('client/build'));
+
+    App.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','index.html'));
+    } )
+}
 
 const PORT=5000 || process.env.PORT;
 App.listen(PORT,()=>console.log(`server started on ${PORT}`))
